ce140df5d20c36104bc3b327f3dbde41cc04e107
Dm4rt <danmartinez2004@gmail.com>
2022-08-29 22:21:39 -0400

    fixed comment part of assignment

--- smaller.cpp
+++ smaller.cpp
@@ -7,7 +7,7 @@ Instructor: Gennadiy Maryash
 Asignment: Lab1A
 
 This program asks the user to input two numbers.
-The first number is store in the variable firstNum.
+The first number is stored in the variable firstNum.
 The second number is stored in the variable secondNum.
 Afterwards, an if statement checks to see which of the two variables is smaller.
 If firstNum is smaller, the variable smallerNum stores the value of firstNum,
@@ -21,7 +21,6 @@ int main()
   
   std:: cout<<"Enter the first number:";
   std:: cin >>firstNum;
-  //std:: cout<<"\n";
   std:: cout<<"Enter the second number:";
   std:: cin >>secondNum;
   std:: cout<<"\n";
