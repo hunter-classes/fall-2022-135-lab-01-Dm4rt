d1481787a7b204c94d0ddd3a47c693a0ed5cbf6e
Dm4rt <danmartinez2004@gmail.com>
2022-08-29 22:52:13 -0400

    finished task A

--- smaller.cpp
+++ smaller.cpp
@@ -4,7 +4,7 @@
 Author:Daniel Martinez
 Course: CSCI-135
 Instructor: Gennadiy Maryash
-Asignment: Lab1B
+Asignment: Lab1A
 
 This program asks the user to input two numbers.
 The first number is stored in the variable firstNum.
