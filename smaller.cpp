7f35e35f57791d73b44ae8d49bb4fbd5bd962a00
Dm4rt <danmartinez2004@gmail.com>
2022-08-29 22:16:22 -0400

    added task A

--- smaller.cpp
+++ smaller.cpp
@@ -5,6 +5,14 @@ Author:Daniel Martinez
 Course: CSCI-135
 Instructor: Gennadiy Maryash
 Asignment: Lab1A
+
+This program asks the user to input two numbers.
+The first number is store in the variable firstNum.
+The second number is stored in the variable secondNum.
+Afterwards, an if statement checks to see which of the two variables is smaller.
+If firstNum is smaller, the variable smallerNum stores the value of firstNum,
+else smallerNum stores the value of secondNum.
+smallerNum is the variable that is printed in the end.
  */
 int main()
 {
